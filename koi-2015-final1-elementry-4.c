cdb6063d07dbdf14c0652a3de1060180e1a47f45
Devonnuri <devonnuri@gmail.com>
2016-04-12 19:39:16 +0900

    Update - Cpp to C and Indentation

--- koi-2015-final1-elementry-4.c
+++ koi-2015-final1-elementry-4.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-using namespace std;
-
 struct str_ball{
     int b_num, b_color, b_size, b_score;
 } ball[200000];
@@ -9,13 +7,13 @@ struct str_ball{
 int main()
 {
     int i, j, n;
-
+    
     scanf("%d", &n);
     for(i=1; i<=n; i++){
         scanf("%d %d", &ball[i].b_color, &ball[i].b_size);
         ball[i].b_num = i;
     }
-
+    
     for(i=1; i<=n; i++){
         for(j=1; j<=n; j++){
             if(i!=j){
@@ -23,10 +21,10 @@ int main()
             }
         }
     }
-
+    
     for(i=1; i<=n; i++){
         printf("%d\n", ball[i].b_score);
     }
-
+    
     return 0;
 }
